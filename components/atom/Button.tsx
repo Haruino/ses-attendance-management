d89956ccc0274085b2fac7bc75b5d26ec7b6646f
Haruino <harukiinoue5270@gmail.com>
2025-03-15 13:55:31 +0900

    fix

--- components/atom/Button.tsx
+++ components/atom/Button.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/tailwindUtils"
 import { cva } from "class-variance-authority"
 import React from "react"
 
--- lib/user.ts
+++
@@ -1,6 +0,0 @@
-import apiClient from "./axiosInstance"
-
-export const getUser = async () => {
-  const { data } = await apiClient.get("/user")
-  return data
-}
